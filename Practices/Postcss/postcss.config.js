cf6175f57d2119138717ec457b164534e1d5c029
SleeplessN <gmlwns7020@naver.com>
2022-07-25 16:54:18 +0900

    Postcss 사용 예시

---
+++ Practices/Postcss/postcss.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  plugins: {
+    // cssnano: {},
+    "postcss-pxtorem": {},
+    "postcss-combine-media-query": {},
+    "postcss-import": {},
+    "postcss-preset-env": { stage: 0 },
+  },
+};
